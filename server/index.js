c59a034542688c03c4ff04deb4f7c24969460107
Nasif <nasifuad007@gmail.com>
2025-01-21 07:28:26 +0600

    e

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(errorMiddleware);
 (async () => {
   try {
     await connectDB();
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.error("Failed to connect to the database:", error.message);
