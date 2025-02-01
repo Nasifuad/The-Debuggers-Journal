27d53c18661ed8d5ad575d2a128da637019b02ca
Nasif <nasifuad007@gmail.com>
2025-02-01 07:40:44 +0600

    manual env file added

--- server_prof/src/utils/cloudinary.js
+++ server_prof/src/utils/cloudinary.js
@@ -1,11 +1,49 @@
+// import dotenv from "dotenv";
 // import { v2 as cloudinary } from "cloudinary";
+// import fs from "fs";
+// // Load environment variables
+// dotenv.config({ path: "./cloud.env" });
+
+// // Configure Cloudinary
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME || "dt2wvbs5q",
+//   api_key: process.env.CLOUD_API_KEY || 841895357524659,
+//   api_secret: process.env.CLOUD_API_SECRET || "hq7GBoXBUANKYO6tXv-OXXBqsLo",
+// });
+
+// export const uploadCloudinary = async (localFilePath) => {
+//   try {
+//     if (!localFilePath) return null;
+
+//     // Upload the file to Cloudinary
+//     const response = await cloudinary.uploader.upload(localFilePath, {
+//       resource_type: "auto",
+//     });
+//     fs.unlink(localFilePath, (err) => {
+//       if (err) {
+//         console.error("Error deleting local file:", err);
+//       }
+//     });
+//     return response.url;
+//   } catch (error) {
+//     fs.unlink(localFilePath, (err) => {
+//       if (err) {
+//         console.error("Error deleting local file:", err);
+//       }
+//     });
+//     console.error("Error uploading file to Cloudinary:", error);
+//     return null;
+//   }
+// };
+
+import { v2 as cloudinary } from "cloudinary";
 import { Readable } from "stream";
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME || "dt2wvbs5q",
+  api_key: process.env.CLOUD_API_KEY || 841895357524659,
+  api_secret: process.env.CLOUD_API_SECRET || "hq7GBoXBUANKYO6tXv-OXXBqsLo",
 });
 
 // Upload buffer to Cloudinary
