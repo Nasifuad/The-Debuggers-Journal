368c0bbf85d904855a8c27ed1dce7a6d7c4cc0c7
Nasif <nasifuad007@gmail.com>
2025-02-01 07:11:08 +0600

    e

--- server_prof/src/app.js
+++ server_prof/src/app.js
@@ -11,12 +11,7 @@ const allowedOrigins = [
   "https://the-debuggers-journal-e2f6.vercel.app",
 ];
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
